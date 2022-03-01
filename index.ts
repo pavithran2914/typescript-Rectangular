494d390571f2d09e9e6079188dd9a067d44e5077
pavithran2914 <pavithranbala29@gmail.com>
2022-03-01 12:17:55 +0530

    1 file modified

--- index.ts
+++ index.ts
@@ -1,10 +1,7 @@
 // Import stylesheets
 import './style.css';
 import { Rectangular } from './rectangular.model';
-import { Dot } from './dot.model';
 import { Coordinate } from './coordinate.model';
-import { Line } from './line.model';
-
 
 // Write TypeScript code!
 const appDiv: HTMLElement = document.getElementById('app');
