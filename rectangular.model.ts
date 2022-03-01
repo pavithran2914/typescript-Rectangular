0a9d65ad41de77dc33f07e529376c80fe1c1b46f
pavithran2914 <pavithranbala29@gmail.com>
2022-03-01 15:12:27 +0530

    2 files modified

--- rectangular.model.ts
+++ rectangular.model.ts
@@ -12,21 +12,13 @@ export class Rectangular {
     this.line();
   }
   dot() {
-    let dot1 = `${this.coordinate.x}, ${this.coordinate.y}`;
-    let dot2 = `${this.coordinate.x + this.w}, ${this.coordinate.y}`;
-    let dot3 = `${this.coordinate.x }, ${this.coordinate.y + this.h}`;
-    let dot4 = `${this.coordinate.x + this.w}, ${this.coordinate.y + this.h}`;
     console.log(
-      `Dot1 = ${dot1} \n Dot2 = ${dot2} \n Dot3 = ${dot3} \n Dot4 = ${dot4}`
+      `Dot1 = ${this.coordinate.x}, ${this.coordinate.y} \n Dot2 = ${this.coordinate.x + this.w}, ${this.coordinate.y} \n Dot3 = ${this.coordinate.x}, ${this.coordinate.y + this.h} \n Dot4 = ${this.coordinate.x + this.w},                   ${this.coordinate.y + this.h}`
     );
   }
   line() {
-    let dot1 = `${this.coordinate.x}, ${this.coordinate.y}`;
-    let dot2 = `${this.coordinate.x + this.w}, ${this.coordinate.y}`;
-    let dot3 = `${this.coordinate.x }, ${this.coordinate.y + this.h}`;
-    let dot4 = `${this.coordinate.x + this.w}, ${this.coordinate.y + this.h}`;
     console.log(
-      `Line1 = ${dot1} to ${dot2} \n Line2 = ${dot2} to ${dot4} \n Line3 = ${dot4} to ${dot3} \n Line4 = ${dot3} to ${dot1}`
+      `Line1 = ${this.coordinate.x}, ${this.coordinate.y} to ${this.coordinate.x + this.w}, ${this.coordinate.y} \n Line2 = ${this.coordinate.x + this.w}, ${this.coordinate.y} to ${this.coordinate.x + this.w}, ${this.coordinate.y + this.h} \n Line3 = ${this.coordinate.x + this.w}, ${this.coordinate.y + this.h} to ${this.coordinate.x}, ${this.coordinate.y + this.h} \n Line4 = ${this.coordinate.x}, ${this.coordinate.y + this.h} to ${this.coordinate.x}, ${this.coordinate.y}`
     );
   }
 }
